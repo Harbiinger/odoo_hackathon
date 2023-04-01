f27284db5d5695b77d22f3339e48e49bfdf0003b
LicorneRose765 <210376@umons.ac.be>
2023-04-01 16:09:38 +0200

    usernames

--- app/src/main/java/clia/back/fs/Users.java
+++ app/src/main/java/clia/back/fs/Users.java
@@ -5,10 +5,18 @@ public enum Users {
 
     public static Users getUser(String user) {
         return switch (user) {
-            case "Employee" -> Employee;
-            case "Manager" -> Manager;
-            case "CEO" -> CEO;
+            case "user", "Employee" -> Employee;
+            case "mngr", "Manager" -> Manager;
+            case "CEO", "theo" -> CEO;
             default -> null;
+        };
+    }
+    
+    public static String getUsername(Users user) {
+        return switch (user) {
+            case Employee -> "user";
+            case Manager -> "mngr";
+            case CEO -> "theo";
         };
     }
 }
